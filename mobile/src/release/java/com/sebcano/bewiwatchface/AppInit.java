ea3d05078c08580f24234ae6fa41a2a16868a9cf
Ludwig van B <ludwig.van.b@gmail.com>
2017-04-19 10:07:31 +0200

    Add missing source

---
+++ mobile/src/release/java/com/sebcano/bewiwatchface/AppInit.java
@@ -0,0 +1,14 @@
+package com.sebcano.bewiwatchface;
+
+import android.content.Context;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class AppInit {
+
+    public static void onStartup(Context context) {
+    }
+
+    public static void configureLocationApi( GoogleApiClient googleApiClient ) {
+    }
+}
